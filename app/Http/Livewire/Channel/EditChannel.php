bc60ee9a9eb778241981ed21718fa72b9982c457
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-21 20:35:43 +0700

    edit channel

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -9,6 +9,15 @@ class EditChannel extends Component
 {
     public $channel;
 
+    protected function rules () {
+        return [
+            'channel.name' => 'required|max:255|unique:channels,name,' . $this->channel->id,
+            'channel.slug' => 'required|max:255|unique:channels,slug,' . $this->channel->id, 
+            'channel.description' => 'nullable|max:1000',
+        ];
+    }
+
+
     public function mount(Channel $channel) {
         $this->channel = $channel;
     }
@@ -16,5 +25,18 @@ class EditChannel extends Component
     public function render()
     {
         return view('livewire.channel.edit-channel');
+    }
+
+    public function update() {
+        $this->validate();
+
+        $this->channel->update([
+            'name' => $this->channel->name,
+            'slug' => $this->channel->slug,
+            'description' => $this->channel->description,
+        ]);
+
+        session()->flash('message', 'Channel update');
+        return redirect()->route('channel.edit', ['channel' => $this->channel->slug]);
     }
 }
--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -2,18 +2,39 @@
     <form wire:submit.prevent="update">
         <div class="form-group">
             <label for="name">Nama channel</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" wire:model="channel.name">
         </div>
+        @error('channel.name')
+            <div class="text-danger">
+                {{ $message }}
+            </div>
+        @enderror
         <div class="form-group">
             <label for="slug">Slug</label>
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" wire:model="channel.slug">
         </div>
+        @error('channel.slug')
+            <div class="text-danger">
+                {{ $message }}
+            </div>
+        @enderror
         <div class="form-group">
             <label for="description">Deskripsi</label>
-            <textarea cols="30" rows="4" class="form-control"></textarea>
+            <textarea cols="30" rows="4" class="form-control" wire:model="channel.description"></textarea>
         </div>
+        @error('channel.description')
+            <div class="text-danger">
+                {{ $message }}
+            </div>
+        @enderror
         <div class="form-group">
             <button type="submit" class="btn btn-primary">Update</button>
         </div>
+        @if (session()->has('message'))
+            <div class="text-success">
+                {{ session('message') }}
+            </div>
+            
+        @endif
     </form>
 </div>
