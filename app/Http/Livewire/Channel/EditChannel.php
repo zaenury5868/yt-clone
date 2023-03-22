c72c2784d46550b23ed20e02e91129a8575f8d46
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-22 20:59:06 +0700

    upload file livewire

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -5,17 +5,21 @@ namespace App\Http\Livewire\Channel;
 use App\Models\Channel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class EditChannel extends Component
 {
     use AuthorizesRequests;
+    use WithFileUploads;
     public $channel;
+    public $image;
 
     protected function rules () {
         return [
             'channel.name' => 'required|max:255|unique:channels,name,' . $this->channel->id,
             'channel.slug' => 'required|max:255|unique:channels,slug,' . $this->channel->id, 
             'channel.description' => 'nullable|max:1000',
+            'image' => 'nullable|image|max:1024',
         ];
     }
 
@@ -30,6 +34,7 @@ class EditChannel extends Component
     }
 
     public function update() {
+
         $this->authorize('update', $this->channel);
         $this->validate();
 
@@ -38,6 +43,17 @@ class EditChannel extends Component
             'slug' => $this->channel->slug,
             'description' => $this->channel->description,
         ]);
+
+        //check file submitted
+        if($this->image) {
+            //save image
+            $image = $this-> image->storeAs('image', $this->channel->uid . '.png');
+
+            //update file path in the db
+            $this->channel->update([
+                'image' => $image
+            ]);
+        }
 
         session()->flash('message', 'Channel update');
         return redirect()->route('channel.edit', ['channel' => $this->channel->slug]);
--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -1,6 +1,6 @@
 <div>
     <form wire:submit.prevent="update">
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="name">Nama channel</label>
             <input type="text" class="form-control" wire:model="channel.name">
         </div>
@@ -9,7 +9,8 @@
                 {{ $message }}
             </div>
         @enderror
-        <div class="form-group">
+
+        <div class="form-group mb-2">
             <label for="slug">Slug</label>
             <input type="text" class="form-control" wire:model="channel.slug">
         </div>
@@ -18,15 +19,32 @@
                 {{ $message }}
             </div>
         @enderror
-        <div class="form-group">
+
+        <div class="form-group mb-2">
             <label for="description">Deskripsi</label>
             <textarea cols="30" rows="4" class="form-control" wire:model="channel.description"></textarea>
         </div>
         @error('channel.description')
+        <div class="text-danger">
+            {{ $message }}
+        </div>
+        @enderror
+
+        <div class="form-group mb-2">
+            <input type="file" wire:model="image">
+        </div>
+        <div class="form-group">
+            @if ($image)
+                Preview :
+                <img src="{{ $image->temporaryUrl() }}" class="img-thumbnail">
+            @endif
+        </div>
+        @error('image')
             <div class="text-danger">
                 {{ $message }}
             </div>
         @enderror
+
         <div class="form-group">
             <button type="submit" class="btn btn-primary">Update</button>
         </div>
