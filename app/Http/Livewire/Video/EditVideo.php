3d97efe4c6f645ffea2569e019d2d6d76fe689d4
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-24 17:47:58 +0700

    edit record video

--- app/Http/Livewire/Video/EditVideo.php
+++ app/Http/Livewire/Video/EditVideo.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Livewire\Video;
 
+use App\Models\Video;
+use App\Models\Channel;
 use Livewire\Component;
 
 class EditVideo extends Component
 {
+    public Channel $channel;
+    public Video $video;
+    public $videoFile;
+
+    protected $rules = [
+        'video.title' => 'required|max:255',
+        'video.description' => 'nullable|max:1000',
+        'video.visibility' => 'required|in:private,public,unlisted'
+    ];
+
+    public function mount($channel, $video) {
+        $this->channel = $channel;
+        $this->video = $video;
+    }
     public function render()
     {
         return view('livewire.video.edit-video')->extends('layouts.app');
+    }
+
+    public function update() {
+        $this->validate();
+
+        $this->video->update([
+            'title' => $this->video->title,
+            'description' => $this->video->description,
+            'visibility' => $this->video->visibility,
+        ]);
+
+        session()->flash('message', 'video was update');
     }
 }
--- resources/views/livewire/video/edit-video.blade.php
+++ resources/views/livewire/video/edit-video.blade.php
@@ -1,3 +1,54 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form wire:submit.prevent="update">
+                    <div class="form-group mb-2">
+                        <label for="title">Judul</label>
+                        <input type="text" class="form-control" wire:model="video.title">
+                    </div>
+                    @error('video.title')
+                        <div class="text-danger">
+                            {{ $message }}
+                        </div>
+                    @enderror
+            
+                    <div class="form-group mb-2">
+                        <label for="description">Deskripsi</label>
+                        <textarea cols="30" rows="4" class="form-control" wire:model="video.description"></textarea>
+                    </div>
+                    @error('video.description')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                    @enderror
+            
+                    <div class="form-group mb-2">
+                        <label for="visibility">Visibility</label>
+                        <select wire:model="video.visibility" class="form-control">
+                            <option value="private">Private</option>
+                            <option value="public">Public</option>
+                            <option value="unlisted">Unlisted</option>
+                        </select>
+                    </div>
+                    @error('video.visibility')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                    @enderror
+            
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </div>
+                    @if (session()->has('message'))
+                        <div class="text-success">
+                            {{ session('message') }}
+                        </div>
+                        
+                    @endif
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
