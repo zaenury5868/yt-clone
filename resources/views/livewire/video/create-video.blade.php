9b1ff5ce3710d276183d77014790bb71aa3ec6bd
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-24 16:37:36 +0700

    add progress bar uploding video

--- resources/views/livewire/video/create-video.blade.php
+++ resources/views/livewire/video/create-video.blade.php
@@ -2,11 +2,23 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card" x-data="{isUploading: false, progress: 0}"
+                    x-on:livewire-upload-start="isUploading = true"
+                    x-on:livewire-upload-finish="isUploading = false, $wire.fileCompleted()"
+                    x-on:livewire-upload-error="isUploading = false"
+                    x-on:livewire-upload-progress="progress = $event.detail.progress">
                     <div class="card-body">
-                        <form action="">
-                            <input type="file" wire:model='videoFile' wire:change="upload">
+                        <div class="progress my-4" x-show="isUploading">
+                            <div class="progress-bar" role="progressbar" :style="`width: ${progress}%`"></div>
+                        </div>
+                        <form x-show="!isUploading">
+                            <input type="file" wire:model='videoFile'>
                         </form>
+                        @error('videoFile')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
             </div>
