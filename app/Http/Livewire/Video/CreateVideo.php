f05985a65c550e66b0d2cf7a6b85c445b28fccb9
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-24 16:18:56 +0700

    upload video to temporary folder with livewire

--- app/Http/Livewire/Video/CreateVideo.php
+++ app/Http/Livewire/Video/CreateVideo.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Livewire\Video;
 
+use App\Models\Video;
+use App\Models\Channel;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreateVideo extends Component
 {
-    public $name = "dgfdg";
+    use WithFileUploads;
+    public Channel $channel;
+    public Video $video;
+    public $videoFile;
+
+    public function mount(Channel $channel) {
+        $this->channel = $channel;
+    }
+
     public function render()
     {
         return view('livewire.video.create-video')->extends('layouts.app');
+    }
+
+    public function fileCompleted() {
+    }
+    
+    public function upload() {
+        $this->validate([
+            'videoFile' => 'required|mimes:mp4|max:102400'
+        ]);
     }
 }
--- resources/views/livewire/video/create-video.blade.php
+++ resources/views/livewire/video/create-video.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{ $name }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="">
+                            <input type="file" wire:model='videoFile' wire:change="upload">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
