a7d145046532d44d674986010e81c6105885c4be
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-25 13:18:39 +0700

    dispatch video encoding job

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -3,12 +3,13 @@
 namespace App\Jobs;
 
 use App\Models\Video;
+use FFMpeg\Format\Video\X264;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class ConvertVideoForStreaming implements ShouldQueue
@@ -29,6 +30,30 @@ class ConvertVideoForStreaming implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $destination = '/' . $this->video->uid . '/' . $this->video->uid . '.m3u8';
+        $low = (new X264('aac'))->setKiloBitrate(500);
+        $high = (new X264('aac'))->setKiloBitrate(1000);
+
+        FFMpeg::fromDisk('videos-temp')
+            ->open($this->video->path)
+            ->exportForHLS()
+            ->addFormat($low, function($filters) {
+                $filters->resize(640, 480);
+            })
+            ->onProgress(function ($progress) {
+                $this->video->update([
+                    'processing_percentage' => $progress
+                ]);
+            })
+            ->addFormat($high, function($filters) {
+                $filters->resize(1280, 720);
+            })
+            ->toDisk('videos')
+            ->save($destination);
+
+            $this->video->update([
+                'processed' => true,
+                'processed_file' => $this->video->uid . '.m3u8',
+            ]);
     }
 }
