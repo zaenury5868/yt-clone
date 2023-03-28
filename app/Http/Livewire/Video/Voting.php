70726f354444b4608605a8d985aafb31850a9ad6
Zaenury Dhany Wibowo <zaenury5868@gmail.com>
2023-03-28 15:27:07 +0700

    Toggle Like/Dislike - one is active at a time

--- app/Http/Livewire/Video/Voting.php
+++ app/Http/Livewire/Video/Voting.php
@@ -46,6 +46,7 @@ class Voting extends Component
             $this->video->likes()->create([
                 'user_id' => auth()->id()
             ]);
+            $this->disableDislike();
             $this->likeActive = true;
         }
         $this->emit('load_values');
@@ -59,9 +60,19 @@ class Voting extends Component
             $this->video->dislikes()->create([
                 'user_id' => auth()->id()
             ]);
+            $this->disableLike();
             $this->dislikeActive = true;
         }
         $this->emit('load_values');
+    }
 
+    public function disableLike() {
+        Like::where('user_id', auth()->id())->where('video_id', $this->video->id)->delete();
+        $this->likeActive = false;
+    }
+
+    public function disableDislike() {
+        Dislike::where('user_id', auth()->id())->where('video_id', $this->video->id)->delete();
+        $this->dislikeActive = false;
     }
 }
